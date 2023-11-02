19d687fbc1783a6de1134a862789a26f9f49dc3f
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2023-11-02 09:31:18 +0100

    Added Function to the Calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let output = "";
+
+    const Select = (value) => () => {
+        output = document.querySelector(".OutputField").value;
+        output += value;
+    };
+
+    const CalculateString = () => {
+        try {
+            output = document.querySelector(".OutputField").value;
+            output = eval(output);
+        } catch (error) {
+            output = "Error";
+        }
+    };
+</script>
+
+<div class="CalculatorContainer">
+    <input type="text" class="OutputField" value={output} />
+    <div class="ButtonContainer">
+        <button on:click={Select(1)}>1</button>
+        <button on:click={Select(2)}>2</button>
+        <button on:click={Select(3)}>3</button>
+        <button on:click={Select("+")} class="Operator">+</button>
+        <button on:click={Select(4)}>4</button>
+        <button on:click={Select(5)}>5</button>
+        <button on:click={Select(6)}>6</button>
+        <button on:click={Select("-")} class="Operator">-</button>
+        <button on:click={Select(7)}>7</button>
+        <button on:click={Select(8)}>8</button>
+        <button on:click={Select(9)}>9</button>
+        <button on:click={Select("*")} class="Operator">*</button>
+        <button on:click={Select(0)} class="ZeroButton">0</button>
+        <button on:click={Select(".")}>.</button>
+        <button on:click={Select("/")} class="Operator">/</button>
+        <button on:click={CalculateString} class="EqualButton">=</button>
+    </div>
+</div>
+
+<style>
+    .CalculatorContainer {
+        width: 300px;
+        height: 400px;
+    }
+
+    .OutputField {
+        width: 100%;
+    }
+
+    button {
+        width: 22%;
+        margin: 1px 1%;
+    }
+
+    .buttonContainer {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    .Operator {
+        background-color: #ff0000;
+    }
+
+    .ZeroButton {
+        width: 46.5%;
+    }
+
+    .EqualButton {
+        width: 100%;
+    }
+</style>
